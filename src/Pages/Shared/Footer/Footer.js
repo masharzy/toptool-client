8155b25f0bd7747980abac5e338f37ebbea8db68
masharzy <horsnbd@gmail.com>
2022-05-24 21:50:56 +0600

    Footer Done

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,20 +1,27 @@
-import { faEnvelope, faLocation, faLocationArrow, faPhone } from "@fortawesome/free-solid-svg-icons";
+import {
+  faEnvelope,
+  faLocation,
+  faLocationArrow,
+  faPhone,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer class="footer p-10 bg-base-200 text-base-content">
-      <div>
+    <footer class="footer w-full px-24 py-10 bg-[#1E2B47] text-white">
+      <div className="text-base">
         <h2 className="text-4xl font-bold">TOPTOOL</h2>
         <p className="text-left">
-          Toptools.com.bd is an online store of quality <br /> professional
-          tools with one of the largest ranges <br /> of power tools, hand
-          tools, and cordless power <br /> tools.
+          Toptools.com.bd is an online store of quality <br className="hidden lg:block" /> professional
+          tools with one of the largest ranges <br className="hidden lg:block" /> of power tools, hand
+          tools, and cordless power <br className="hidden lg:block" /> tools.
         </p>
         <ul className="text-left leading-6">
           <li>
-            <FontAwesomeIcon icon={faLocationArrow} /> Lahapara, Sonargaon, Narayanganj, Dhaka
+            <FontAwesomeIcon icon={faLocationArrow} /> Lahapara, Sonargaon,
+            Narayanganj, Dhaka
           </li>
           <li>
             <FontAwesomeIcon icon={faPhone} /> 01639722575
@@ -24,25 +31,52 @@ const Footer = () => {
           </li>
         </ul>
       </div>
-      <div>
-        <span class="footer-title">Services</span>
-        <a class="link link-hover">Branding</a>
-        <a class="link link-hover">Design</a>
-        <a class="link link-hover">Marketing</a>
-        <a class="link link-hover">Advertisement</a>
+      <div className="text-base">
+        <span class="font-bold text-primary text-xl">
+          USEFUL LINKS
+        </span>
+        <Link to="/" class="link link-hover">
+          Delivery Information
+        </Link>
+        <Link to="/" class="link link-hover">
+          Returns Policy
+        </Link>
+        <Link to="/" class="link link-hover">
+          Privacy Policy
+        </Link>
+        <Link to="/" class="link link-hover">
+          Terms & Conditions
+        </Link>
+        <Link to="/" class="link link-hover">
+          FAQs
+        </Link>
+      </div>
+      <div className="text-base">
+        <span class="font-bold font-mono text-primary text-xl">
+          CUSTOMER SUPPORT
+        </span>
+        <Link to="/" class="link link-hover">
+          My Account
+        </Link>
+        <Link to="/" class="link link-hover">
+          Orders
+        </Link>
+        <Link to="/" class="link link-hover">
+          Checkout
+        </Link>
+        <Link to="/" class="link link-hover">
+          Contact Us
+        </Link>
       </div>
       <div>
-        <span class="footer-title">Company</span>
-        <a class="link link-hover">About us</a>
-        <a class="link link-hover">Contact</a>
-        <a class="link link-hover">Jobs</a>
-        <a class="link link-hover">Press kit</a>
-      </div>
-      <div>
-        <span class="footer-title">Legal</span>
-        <a class="link link-hover">Terms of use</a>
-        <a class="link link-hover">Privacy policy</a>
-        <a class="link link-hover">Cookie policy</a>
+        <span class="font-bold font-mono text-primary text-xl">
+          PAYMENT METHOD
+        </span>
+        <img
+          className="w-52"
+          src="https://i.ibb.co/K5QXmym/TT-Payment-Method-1-4-Outline.png"
+          alt=""
+        />
       </div>
     </footer>
   );
