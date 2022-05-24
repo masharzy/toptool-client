a4e4ee15e4fc2e0f28266efeb498577f9ac18d4f
masharzy <horsnbd@gmail.com>
2022-05-24 21:19:17 +0600

    Reviews Done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Header from "./Pages/Shared/Header/Header";
 import Home from "./Pages/Home/Home";
+import Footer from "./Pages/Shared/Footer/Footer";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>} />
       </Routes>
+      <Footer/>
     </div>
   );
 }
